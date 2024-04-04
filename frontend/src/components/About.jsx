20a3755521997a7143d71db620b6cb8e4953aa24
Ramkrishna goud <ramakrishnagoud9199@gmail.com>
2024-04-05 00:10:23 +0530

    first commit

---
+++ frontend/src/components/About.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="about container">
+      <h4>Events</h4>
+      <h2>ABOUT</h2>
+      <p>
+      We serve as a one-stop destination for individuals and organizations seeking to organize memorable and successful events. With intuitive features such as event listings, venue management, vendor services, and budgeting tools, users can efficiently plan every aspect of their events. From booking venues and vendors to managing guest lists and coordinating logistics, the website streamlines the entire event planning process. With customizable options, secure payment processing, and collaboration features, users can create unique and tailored experiences for their attendees while staying organized and on budget. Whether planning a corporate conference, wedding celebration, or community fundraiser, an event planning website provides the tools and resources needed to bring visions to life and create unforgettable moments.
+      </p>
+    </section>
+  );
+};
+
+export default About;
